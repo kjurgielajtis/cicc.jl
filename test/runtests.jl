12bb37f0bffcf5662d209dca9e00f29a433e8154
kjurgielajtis <jurgielajtiskrzysztof@gmail.com>
2020-05-10 23:09:51 +0200

    Add testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,10 @@
 using cicc
 using Test
 
-@testset "cicc.jl" begin
-    @test example_func(2) == 4
-    @test example_func(-2) == 4
+@testset "secant.jl" begin
+    x, f = 1, x -> x^3
+    @test my_secant(f, -2, 2, 1000) == 0
+
+    x, f = 1, x -> x + 1
+    @test my_secant(f, -10, 10, 1000) == -1.0
 end
