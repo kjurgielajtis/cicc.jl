5eab78d58c273d547a240c72958164605dfd76b0
kjurgielajtis <jurgielajtiskrzysztof@gmail.com>
2020-05-10 23:09:34 +0200

    Add secant method function

--- src/cicc.jl
+++ src/cicc.jl
@@ -1,7 +1,34 @@
 module cicc
 
-example_func(x) = x^2
+"Returns root of given function using secant method"
+function my_secant(f, x1, x2, eps)
+    xm = 0
+    x0 = 0
+    c = 0
 
-export example_func
+    if(f(x1) * f(x2) < 0)
+        while true
+            x0 = (x1 * f(x2) - x2 * f(x1)) / (f(x2) - f(x1))
+            c = f(x1) * f(x0)
+
+            x1 = x2
+            x2 = x0
+
+            if(c == 0)
+                break
+            end
+
+            xm = (x1 * f(x2) - x2 * f(x1)) / (f(x2) - f(x1))
+
+            if(abs(xm - x0) < eps)
+                break
+            end
+        end
+        return x0
+    else println("Can not find a root")
+    end
+end
+
+export my_secant
 
 end # module
